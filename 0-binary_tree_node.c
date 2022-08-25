a88ee3a967bcfa317ffed2e2c5e2f40aa9e50613
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2022-08-25 12:44:50 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,19 +7,18 @@
  * Return: the new node
  */
 
- binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
- {
-     binary_tree_t *node;
-     
-     node = malloc(sizeof(binary_tree_t));
-     if (node == NULL)
-     {
-         return (NULL);
-     }
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
 
-     node->n = value;
-     node->parent = parent;
-     node->left = NULL;
-     node->right = NULL;
-     return (node);
- }
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
+}
